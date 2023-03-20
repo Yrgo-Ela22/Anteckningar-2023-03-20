455d75b433bf91b71631a923144f846abbc037af
Erik Pihl <100573213+ErikPihl@users.noreply.github.com>
2023-03-20 19:06:22 +0100

    Laddade upp filer

---
+++ isr.c
@@ -0,0 +1,29 @@
+/********************************************************************************
+* isr.c: Innehåller avbrottsrutiner.
+********************************************************************************/
+#include "display.h"
+
+/********************************************************************************
+* ISR (TIMER1_COMPA_vect): Avbrottsrutin som äger rum vid uppräkning till 256 av
+*                          Timer 1 i CTC Mode, vilket sker var 0.128:e
+*                          millisekund när timern är aktiverad. En gång per
+*                          millisekund togglas talet utskrivet på 
+*                          7-segmentsdisplayerna mellan tiotal och ental.
+********************************************************************************/
+ISR (TIMER1_COMPA_vect)
+{
+   display_toggle_digit();
+   return;
+}
+
+/********************************************************************************
+* ISR (TIMER2_OVF_vect): Avbrottsrutin som äger rum vid uppräkning till 256 av
+*                        Timer 2 i Normal Mode, vilket sker var 0.128:e
+*                        millisekund när timern är aktiverad. En gång per sekund
+*                        inkrementeras talet utskrivet på 7-segmentsdisplayerna.
+********************************************************************************/
+ISR (TIMER2_OVF_vect)
+{
+   display_count();
+   return;
+}
---
+++ main.c
@@ -0,0 +1,46 @@
+/********************************************************************************
+* main.c: Demonstration av inbyggt system innefattande 7-segmentsdisplayer.
+*         Timerkrets Timer 1 används för att räkna upp befintligt tal på
+*         7-segmentsdisplayerna en gång per sekund.
+********************************************************************************/
+#include "timer.h"
+#include "wdt.h"
+#include "display.h"
+
+/********************************************************************************
+* setup: Initierar systemet enligt följande:
+*
+*        1. Initierar Watchdog-timern med en timeout på 1024 ms. System reset
+*           aktiveras så att systemåterställning sker ifall Watchdog-timern
+*           löper ut.
+*
+*        2. Initierar 7-segmentsdisplayerna med startvärde 0 och aktiverar
+*           uppräkning en gång per sekund.
+********************************************************************************/
+static inline void setup(void)
+{
+   wdt_init(WDT_TIMEOUT_1024_MS);
+   wdt_enable_interrupt();
+
+   display_init();
+   display_enable_output();
+   display_enable_count();
+   return;
+}
+
+/********************************************************************************
+* main: Initierar systemet vid start. Uppräkning sker sedan kontinuerligt
+*       av talet på 7-segmentsdisplayerna en gång per sekund.
+********************************************************************************/
+int main(void)
+{
+   setup();
+   
+   while (1)
+   {
+      wdt_reset();
+   }
+
+   return 0;
+}
+
